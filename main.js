d4b526d04726ad52039992e3d2ddd34265335804
Maya Valencia <valem023@newschool.edu>
2021-02-19 16:32:01 -0500

    animated spinning text with scroll

---
+++ main.js
@@ -0,0 +1,11 @@
+/*scroll function*/
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
