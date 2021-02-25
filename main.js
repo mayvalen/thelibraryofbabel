a4f95de9d9c7e4e9dcdf13b294982cb71bb972a1
Maya Valencia <valem023@newschool.edu>
2021-02-25 11:57:58 -0500

    scrambled paragraphs, half story in

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-/*scroll function*/
+function myFunction() {
+  var x = document.getElementById("firstparagraph");
+  var y = document.getElementById("scramble");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+
+/*scroll function
 window.addEventListener(
   "scroll",
   () => {
@@ -8,4 +20,5 @@ window.addEventListener(
     );
   },
   false
-);
+);*/
+
